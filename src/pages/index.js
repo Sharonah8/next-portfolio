1849a9575a330e9aa39c640535a88a1c0194f98f
Snyamongo <Snyamongo@safaricom.co.ke>
2023-09-07 12:18:43 +0300

    Add global styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import Hero from "@/components/Hero";
-import About from "@/components/About";
-import Skills from "@/components/Skills";
-import Projects from "@/components/Projects";
-import Contact from "@/components/Contact";
+// import Hero from "@/components/Hero";
+// import About from "@/components/About";
+// import Skills from "@/components/Skills";
+// import Projects from "@/components/Projects";
+// import Contact from "@/components/Contact";
 
 import Head from "next/head";
 
@@ -16,11 +16,11 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <Hero />
+        {/* <Hero />
         <About />
         <Skills />
         <Projects />
-        <Contact />
+        <Contact /> */}
       </div>
     </>
   );
